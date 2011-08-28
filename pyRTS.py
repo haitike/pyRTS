dc4539a1dcc440cf939471279c500430e9dfa40a
haitike <haitike@gmail.com>
2011-08-28 14:27:08 +0200

    .

--- pyRTS.py
+++ pyRTS.py
@@ -1,33 +1,26 @@
-
-#program load
-
 import pygame, sys
-pygame.init() #load pygame modules
-size = width, height = 1024, 768 #size of window
+pygame.init()
+size = width, height = 1024, 768
 screen = pygame.display.set_mode(size) #make window
 
 # back texture load
 
 tiless = pygame.image.load('canstock3554313.jpg')
-tamanotextura = tiless.get_width(), tiless.get_height()
-cantidadtexturas = (width/tamanotextura[0])+1, (height/tamanotextura[1])+1
+texture_size = tiless.get_width(), tiless.get_height()
+texture_amount = (width/texture_size[0])+1, (height/texture_size[1])+1
 
 #main loop start
 
 clock=pygame.time.Clock() #make a clock
 while 1: #infinite loop
-	clock.tick(30) #limit framerate to 30 FPS
-	for event in pygame.event.get(): #if something clicked
-		if event.type == pygame.QUIT: #if EXIT clicked
-			sys.exit() #close cleanly
-	screen.fill((0,0,0)) #make redraw background black
-	
-	# back print
-	
-	for ydrw in range(cantidadtexturas[1]):
-		for xdrw in range(cantidadtexturas[0]):
-			screen.blit(tiless,(xdrw*tamanotextura[0],ydrw*tamanotextura[1])) #render the surface into the rectangle
-	
-	# vsync
-	
-	pygame.display.flip() #update the screen
+    clock.tick(30) #limit framerate to 30 FPS
+    for event in pygame.event.get():  #close cleanly
+        if event.type == pygame.QUIT:
+            sys.exit()
+    screen.fill((0,0,0)) #make redraw background black
+
+    # print
+    for ydrw in range(texture_amount[1]):
+        for xdrw in range(texture_amount[0]):
+            screen.blit(tiless,(xdrw*texture_size[0],ydrw*texture_size[1])) #render the surface into the rectangle
+    pygame.display.flip() #update the screen
